de55e708ffaec65ca71b21ef180aa7f2809b05f8
dejana šević <dejana.sevic24@gmail.com>
2026-05-24 16:20:49 +0200

    fix: replace manual csv upload with kaggle api data ingestion #1

--- app/bronze/twitter/handler.py
+++ app/bronze/twitter/handler.py
@@ -1,10 +1,44 @@
-import boto3
 import os
+import zipfile
+import boto3
 from datetime import date, timedelta
 import botocore.exceptions
+import pandas as pd
+import json
+import requests
+import io
+
 
 
 def lambda_handler(event, context):
+
+    """ Lambda function to check if the twitter file for yesterday already exists in the bronze bucket,
+        if not it will load the dataset from kaggle, filter the tweets from yesterday and upload them as a json file to the s3 bucket."""
+
+    # if the file alrady exists in the bronze bucket, skip the upload
+    if already_exists():
+        print("Twitter file already exists in the bronze bucket, no upload needed.")
+        return
+    
+    # if the file doesn't exist, upload the yesterday's data from kaggle
+    else:
+        print("Twitter file does not exist in the bronze bucket, uploading yesterday's data.")
+
+        # stream the dataset from kaggle in chunks to avoid memory issues
+        df_chunks = stream_kaggle_csv_chunks()
+        
+        # filter the dataset to contain only tweets from yesterday
+        filtered_df = filter_yesterday_tweets(df_chunks)
+        
+        # upload tweets as a json file to the S3 bucket
+        upload_as_json(filtered_df)
+        
+
+
+def already_exists() -> bool:
+
+    """Check if the twitter file for yesterday already exists in the bronze bucket."""
+
     s3 = boto3.client("s3")
 
     # get bucket name from environment variable
@@ -12,14 +46,116 @@ def lambda_handler(event, context):
 
     # calculate yesterday's date and  generate the key for the file in S3
     yesterday = date.today() - timedelta(days=1)
-    key = f"bronze/twitter/year={yesterday.year}/month={yesterday.month:02d}/day={yesterday.day:02d}/tweets.csv"
+    key = f"twitter/year={yesterday.year}/month={yesterday.month:02d}/day={yesterday.day:02d}/tweets.json"
 
-    print(f"Checking for file in s3://{twitter_bucket_name}/{key}")
+    print(f"Checking for file in S3://{twitter_bucket_name}/{key}")
     try:
+        # check if the file exists in S3 bucket and return true if it does, otherwise return false
         s3.head_object(Bucket=twitter_bucket_name, Key=key)
-        print(f"File exists in s3://{twitter_bucket_name}/{key}")
+        print(f"File exists in S3://{twitter_bucket_name}/{key}")
+        return True
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
-            raise FileNotFoundError(f"File doest not exists in s3://{twitter_bucket_name}/{key}")
+            print(f"File does not exist in S3://{twitter_bucket_name}/{key}")
+            return False
         else:
             raise e
+
+
+def stream_kaggle_csv_chunks(chunk_size=10_000):
+
+    """Stream the dataset from kaggle in chunks to avoid memory issues."""
+
+    # kaggle api endpoint for downloading the dataset as a zip file
+    url = "https://www.kaggle.com/api/v1/datasets/download/kaushiksuresh147/bitcoin-tweets/Bitcoin_tweets.csv"
+    
+    # get kaggle credentials from environment variables
+    username = os.environ["KAGGLE_USERNAME"]
+    key = os.environ["KAGGLE_KEY"]
+    
+    # stream the zip file from kaggle and read the csv in chunks without saving the full file to disk or holding it in memory
+    with requests.get(url, auth=(username, key), stream=True) as r:
+        # if the request was not successful, raise an error
+        r.raise_for_status()
+        
+        # write the zip to /tmp in chunks and don't load the whole file in memory
+        zip_path = "/tmp/tweets.zip"
+        with open(zip_path, "wb") as f:
+            # take the response in 8MB chunks and write to file
+            for chunk in r.iter_content(chunk_size=8 * 1024 * 1024): 
+                f.write(chunk)
+        
+        # read the csv file from the zip in chunks using pandas and yield each chunk as a dataframe
+        with zipfile.ZipFile(zip_path) as zf:
+            with zf.open("Bitcoin_tweets.csv") as csv_file:
+                buffer = io.TextIOWrapper(csv_file, encoding="utf-8", errors="replace")
+                # read the csv in chunks and yield each chunk as a dataframe
+                for chunk in pd.read_csv(buffer, chunksize=chunk_size):
+                    yield chunk
+
+
+def filter_yesterday_tweets(df_chunks):
+
+    """Filter the dataset to contain only tweets from yesterday."""
+    
+    # calculate yesterday's date
+    yesterday = date.today() - timedelta(days=1)
+    chunks = []
+
+    for chunk in df_chunks:
+        # convert the date column to datetime format and handle parsing errors
+        chunk["date"] = pd.to_datetime(chunk["date"], format="mixed", utc=True, errors="coerce")
+        # drop rows where date parsing failed and resulted in NaT, since we can't filter those rows
+        chunk = chunk.dropna(subset=["date"]) 
+
+        # create a filter to select only wanted rows
+        chunk_filter = chunk["date"].dt.date == yesterday
+
+        # apply filter to the chunk and keep only yesterday's tweets
+        filtered_chunk = chunk[chunk_filter]
+
+        # if there are any data, collect it in list
+        if not filtered_chunk.empty:
+            chunks.append(filtered_chunk)
+
+    # return empty data frame if there are not found any tweets, otherwise concatenate the chunks into a single dataframe and return it
+    if not chunks:
+        print("There is no tweet from yesterday in the dataset.")
+        return pd.DataFrame()
+    
+    return pd.concat(chunks, ignore_index=True)
+
+
+def upload_as_json(df: pd.DataFrame) -> None:
+
+    """Upload the filtered tweet as a json file to the s3 bucket."""
+
+    # get bucket name from environment variable
+    twitter_bucket_name = os.environ.get("BRONZE_TWITTER_BUCKET", "")
+
+    s3 = boto3.client("s3")
+
+    # calculate yesterday's date
+    yesterday = date.today() - timedelta(days=1)
+
+    # generate the key for the file in S3
+    key = f"twitter/year={yesterday.year}/month={yesterday.month:02d}/day={yesterday.day:02d}/tweets.json"
+
+    # if no tweets found, upload an empty json file to S3 bucket to prevent the lambda from running again
+    if df.empty:
+        print("No tweets to upload, creating an empty file.")
+        s3.put_object(Bucket=twitter_bucket_name, Key=key, Body=b"[]",ContentType="application/json")
+        return
+
+    # convert the dataframe to a list of records
+    records = df.to_dict(orient="records")
+
+    # convert the records to json format
+    body = json.dumps(records, default=str).encode("utf-8")
+
+    print(f"Uploading {len(records)} tweets to s3://{twitter_bucket_name}/{key}")
+
+    # upload the data to S3
+    s3.put_object(Bucket=twitter_bucket_name, Key=key, Body=body, ContentType="application/json")
+
+    print("Upload completed")
--- infrastructure/stacks/infrastructure_stack.py
+++ infrastructure/stacks/infrastructure_stack.py
@@ -10,6 +10,7 @@ from aws_cdk import (
     RemovalPolicy,
 )
 from constructs import Construct
+import os
 
 
 class InfrastructureStack(cdk.Stack):
@@ -70,13 +71,9 @@ class InfrastructureStack(cdk.Stack):
 
         # role for twitter
         twitter_role = iam.Role(
-            self,
-            "TwitterRole",
-            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"), 
+            self, "TwitterRole", assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"), 
             managed_policies=[
-                iam.ManagedPolicy.from_aws_managed_policy_name(
-                    "service-role/AWSLambdaBasicExecutionRole"
-                )
+                iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole")
             ],
         )
 
@@ -100,13 +97,21 @@ class InfrastructureStack(cdk.Stack):
                 "MAX_PUTS": "500",  # set a 0 for unlimited S3 PUTs
             },
         )
-
-        # lambda for checking whether the twitter file has been uploaded to s3 or not
-        twitter_fn = lambda_.Function(self, "TwitterIngestor", runtime=lambda_.Runtime.PYTHON_3_12, handler="handler.lambda_handler",
-            code=lambda_.Code.from_asset("../app/bronze/twitter"), role=twitter_role, memory_size=128, timeout=Duration.minutes(1),
-            environment={
-                "BRONZE_TWITTER_BUCKET": bronze_bucket.bucket_name,
-            },
+        
+        # add layer for pandas importing since its not included in the default aws lambda environment
+        pandas_layer = lambda_.LayerVersion.from_layer_version_arn(self, "PandasLayer",
+                                                                   "arn:aws:lambda:eu-central-1:336392948345:layer:AWSSDKPandas-Python312:27",)
+        
+        # add lambda fucntion for twitter data ingestion and filtering
+        twitter_fn = lambda_.Function(self, "TwitterIngestor", runtime=lambda_.Runtime.PYTHON_3_12, handler="handler.lambda_handler", 
+                                    code=lambda_.Code.from_asset("../app/bronze/twitter"), role=twitter_role, layers=[pandas_layer],
+                                    ephemeral_storage_size=cdk.Size.mebibytes(1024),
+                                    memory_size=512,  timeout=Duration.minutes(10),  
+                                    environment={
+                                        "BRONZE_TWITTER_BUCKET": bronze_bucket.bucket_name,
+                                        "KAGGLE_KEY": os.environ.get("KAGGLE_KEY", ""),
+                                        "KAGGLE_USERNAME": os.environ.get("KAGGLE_USERNAME", ""),
+                                    },
         )
 
         # EventBridge Daily Schedule
